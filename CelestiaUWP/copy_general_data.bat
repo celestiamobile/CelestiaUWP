13958937d06136457a6fbc64ea7c76fb3641d49a
Levin Li <lilinfeng303@outlook.com>
2021-10-04 20:02:48 +0800

    Exclude all .txt files

--- CelestiaUWP/copy_general_data.bat
+++ CelestiaUWP/copy_general_data.bat
@@ -5,7 +5,7 @@ SET CELESTIA_TEMP_DIR=%TEMP%\celestia
 IF NOT EXIST %CELESTIA_TEMP_DIR% MKDIR %CELESTIA_TEMP_DIR%
 
 SET EXCLUDE_FILE=%CELESTIA_TEMP_DIR%\EXCLUDE.txt
-echo CMakeLists.txt> "%EXCLUDE_FILE%"
+echo .txt> "%EXCLUDE_FILE%"
 
 SET CELESTIA_ROOT=%OutDir%\CelestiaResources
 SET CELESTIA_REPO_ROOT=%ProjectDir%\..\..\Celestia
@@ -18,7 +18,7 @@ FOR %%D in (images locale scripts shaders) DO (
 	XCOPY "%CELESTIA_REPO_ROOT%\%%D" "%CELESTIA_ROOT%\%%D" /E /I /Y /EXCLUDE:%EXCLUDE_FILE%
 )
 
-FOR %%D in (data extras extras-standard models textures) DO (
+FOR %%D in (data extras extras-standard models textures warp) DO (
 	XCOPY "%CELESTIA_CONTENT_REPO_ROOT%\%%D" "%CELESTIA_ROOT%\%%D" /E /I /Y /EXCLUDE:%EXCLUDE_FILE%
 )
 
