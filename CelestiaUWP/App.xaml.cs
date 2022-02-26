2f50fc1d4efabc4a20ef1fc77ebda533e29ba040
Levin Li <lilinfeng303@outlook.com>
2022-02-25 20:42:11 -0800

    Do not hardcode AppCenterID in code (#17)

--- CelestiaUWP/App.xaml.cs
+++ CelestiaUWP/App.xaml.cs
@@ -34,7 +34,7 @@ namespace CelestiaUWP
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
-            AppCenter.Start("06fcb18f-a477-4b2d-9cd9-f1b37135c16b", typeof(Analytics), typeof(Crashes));
+            AppCenter.Start("APPCENTER-APP-ID", typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnLaunched(LaunchActivatedEventArgs e)
