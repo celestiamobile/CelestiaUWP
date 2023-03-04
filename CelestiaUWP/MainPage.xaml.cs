4de434fa6b8b88dcbb013f8ff26968762ef9a74e
Levin Li <lilinfeng303@outlook.com>
2023-03-04 17:34:12 +0800

    Updates to observer mode dialog

--- CelestiaUWP/MainPage.xaml.cs
+++ CelestiaUWP/MainPage.xaml.cs
@@ -957,7 +957,7 @@ namespace CelestiaUWP
                     });
                 }, new KeyboardAccelerator() { Key = (VirtualKey)(action.Item2 - 32), IsEnabled = false });
             }
-            AppendItem(navigationItem, LocalizationHelper.Localize("Set Observer Mode…"), (s,
+            AppendItem(navigationItem, LocalizationHelper.Localize("Flight Mode"), (s,
                 e) =>
             {
                  ShowObserverMode();
