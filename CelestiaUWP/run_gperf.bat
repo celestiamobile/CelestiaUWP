1fa733903826ba681ca0d42fb930bd490eeec16f
Levin Li <lilinfeng303@outlook.com>
2023-09-19 21:22:51 +0800

    move to run_gperf.bat

---
+++ CelestiaUWP/run_gperf.bat
@@ -0,0 +1,13 @@
+SET SolutionDir=%~1
+
+SET CELESTIA_REPO_ROOT=%SolutionDir%\..\Celestia
+
+cd "%CELESTIA_REPO_ROOT%\src\celscript\legacy"
+"%SolutionDir%\tools\gperf.exe" commands.gperf -m4 --output-file=commands.inc
+
+cd "%CELESTIA_REPO_ROOT%\src\celephem"
+"%SolutionDir%\tools\gperf.exe" customrotation.gperf -m4 --output-file=customrotation.inc
+"%SolutionDir%\tools\gperf.exe" customorbit.gperf -m4 --output-file=customorbit.inc
+
+cd "%CELESTIA_REPO_ROOT%\src\celengine"
+"%SolutionDir%\tools\gperf.exe" location.gperf -m4 --output-file=location.inc
