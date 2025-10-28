e533f1314e8410f56d31a28083e475f5b7e4ca3e
Levin Li <lilinfeng303@outlook.com>
2025-10-28 14:13:59 -0700

    Do not resize normal maps

--- scripts/downsize_textures.bat
+++ scripts/downsize_textures.bat
@@ -13,27 +13,34 @@ GOTO :EOF
 
 :DOWNSIZE_TEXTURE
 FOR /F "delims=" %%D IN ('DIR /B /S "hires\*.jpg" "hires\*.png" "hires\*.dds"') DO (
-    for /f "delims=" %%i in ('magick identify -format "%%A" "%%D"') do (
-        set "ALPHA=%%i"
-    )
 
-    set "HAS_NO_ALPHA=false"
-    if /i "!ALPHA!"=="False" set "HAS_NO_ALPHA=true"
-    if /i "!ALPHA!"=="Undefined" set "HAS_NO_ALPHA=true"
-
-    if "!HAS_NO_ALPHA!"=="true" (
-        ECHO Converting texture with no alpha channel
-        magick "%%D" -resize "%~2x%~2" -define png:exclude-chunks=date,time "%~1\%%~nxD"
-    ) else (
-        ECHO Converting texture with alpha channel
-        magick "%%D" ^( +clone -alpha extract ^) -alpha off -resize "%~2x%~2" -compose CopyOpacity -composite -define png:exclude-chunks=date,time "%~1\%%~nxD"
-    )
-
-    IF ERRORLEVEL 1 (
-        ECHO Failed to convert: %%D
-        EXIT /B 1
+    REM --- Check if filename contains "normal" ---
+    ECHO %%~nxD | findstr /I "normal" >NUL
+    IF !ERRORLEVEL!==0 (
+        ECHO Skipping resize for normal map: %%D
     ) ELSE (
-        ECHO Successfully converted: %%D
+        FOR /F "delims=" %%i IN ('magick identify -format "%%A" "%%D" 2^>NUL') DO (
+            SET "ALPHA=%%i"
+        )
+
+        SET "HAS_NO_ALPHA=false"
+        IF /I "!ALPHA!"=="False" SET "HAS_NO_ALPHA=true"
+        IF /I "!ALPHA!"=="Undefined" SET "HAS_NO_ALPHA=true"
+
+        IF "!HAS_NO_ALPHA!"=="true" (
+            ECHO Converting texture with no alpha channel
+            magick "%%D" -resize "%~2x%~2" -define png:exclude-chunks=date,time "%~1\%%~nxD"
+        ) ELSE (
+            ECHO Converting texture with alpha channel
+            magick "%%D" ^( +clone -alpha extract ^) -alpha off -resize "%~2x%~2" -compose CopyOpacity -composite -define png:exclude-chunks=date,time "%~1\%%~nxD"
+        )
+
+        IF ERRORLEVEL 1 (
+            ECHO Failed to convert: %%D
+            EXIT /B 1
+        ) ELSE (
+            ECHO Successfully converted: %%D
+        )
     )
 )
 GOTO :EOF
